e1e426e87bea2c6b63fb6f83836f22ffda56c4a1
tempe443 <mj.tempe443@gmail.com>
2021-09-26 17:45:09 +0900

    匿名ログインのロジックの実装

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,10 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
+import "firebase/auth";
 import Constants from "expo-constants";
 /* types */
 import { Shop } from "../types/shop";
+import { User, initialUser } from "../types/user";
 
 if (!firebase.apps.length) {
   if (Constants.manifest?.extra)
@@ -17,4 +19,23 @@ export const getShops = async () => {
     .get();
   const shops = snapshot.docs.map((doc) => doc.data() as Shop);
   return shops;
+};
+
+export const signIn = async () => {
+  const userCredential = await firebase.auth().signInAnonymously();
+  if (!userCredential.user) return;
+  const { uid } = userCredential.user;
+  const userDoc = await firebase.firestore().collection("users").doc(uid).get();
+  if (!userDoc.exists) {
+    await firebase.firestore().collection("users").doc(uid).set(initialUser);
+    return {
+      ...initialUser,
+      id: uid,
+    } as User;
+  } else {
+    return {
+      id: uid,
+      ...userDoc.data(),
+    } as User;
+  }
 };
--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   StyleSheet,
   SafeAreaView,
   Text,
   ActivityIndicator,
 } from "react-native";
+import { signIn } from "../lib/firebase";
 
 export const AuthScreen: React.FC = () => {
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await signIn();
+      console.log(user);
+    };
+    fetchUser();
+  }, []);
   return (
     <SafeAreaView style={styles.container}>
       <ActivityIndicator size="large" />
