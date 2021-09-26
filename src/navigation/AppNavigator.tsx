3281ae771e4ac8595236ddc8f0db099cb10aebcc
tempe443 <mj.tempe443@gmail.com>
2021-09-26 15:11:37 +0900

    ログイン中の画面実装

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,7 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 /*navigator */
 import { MainTabNavigator } from "./MainTabNavigator";
+/*screen */
+import { AuthScreen } from "../screens/AuthScreen";
 
 export const AppNavigator = () => {
-  return <NavigationContainer>{<MainTabNavigator />}</NavigationContainer>;
+  const user = null;
+  return (
+    <NavigationContainer>
+      {!user ? <AuthScreen /> : <MainTabNavigator />}
+    </NavigationContainer>
+  );
 };
