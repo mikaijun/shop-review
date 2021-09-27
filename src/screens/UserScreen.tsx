521de6dc8a42ba7505fc0a9bbdb1ff0861e43ef7
tempe443 <mj.tempe443@gmail.com>
2021-09-27 10:10:27 +0900

    ユーザー情報の編集

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { StyleSheet, SafeAreaView, Text } from "react-native";
+import firebase from "firebase";
+import { updateUser } from "../lib/firebase";
+/* components */
+import { Form } from "../components/Form";
+import { Button } from "../components/Button";
+import { Loading } from "../components/Loading";
+/* contexts */
+import { UserContext } from "../contexts/useContexts";
+/* types */
 import { RootStackParamList } from "../types/navigation";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RouteProp } from "@react-navigation/native";
@@ -10,9 +19,32 @@ type Props = {
 };
 
 export const UserScreen: React.FC<Props> = ({ navigation, route }: Props) => {
+  const { user, setUser } = useContext(UserContext);
+  const [name, setName] = useState<string>(user ? user.name : "");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const onSubmit = async () => {
+    setLoading(true);
+    // TODO: 後で直す
+    if (!user) return;
+    if (!user.id) return;
+    const updatedAt = firebase.firestore.Timestamp.now();
+    await updateUser(user.id, { name, updatedAt });
+    setUser({ ...user, name, updatedAt });
+    setLoading(false);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>User Screen</Text>
+      <Form
+        value={name}
+        onChangeText={(text) => {
+          setName(text);
+        }}
+        label="名前"
+      />
+      <Button onPress={onSubmit} text="保存する" />
+      <Loading visible={loading} />
     </SafeAreaView>
   );
 };
@@ -21,7 +53,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
