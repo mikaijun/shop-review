4c43c235d93fcf388ecb04888c6f228c56b6d4b7
tempe443 <mj.tempe443@gmail.com>
2021-10-02 14:28:31 +0900

    アップロード処理

--- src/screens/CreateReviewScreen.tsx
+++ src/screens/CreateReviewScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
-import { StyleSheet, SafeAreaView, Text } from "react-native";
+import { StyleSheet, SafeAreaView, View, Image } from "react-native";
 import firebase from "firebase";
 import { addReview } from "../lib/firebase";
+import { pickImage } from "../lib/image-picker";
 import { UserContext } from "../contexts/useContexts";
 /* components */
 import { IconButton } from "../components/IconButton";
@@ -26,6 +27,8 @@ export const CreateReviewScreen: React.FC<Props> = ({
   const { shop } = route.params;
   const [text, setText] = useState<string>("");
   const [score, setScore] = useState<number>(3);
+  const [imageUri, setImageUri] = useState<string>("");
+
   const { user } = useContext(UserContext);
 
   useEffect(() => {
@@ -55,6 +58,11 @@ export const CreateReviewScreen: React.FC<Props> = ({
     } as Review;
     await addReview(shop.id, review);
   };
+
+  const onPickImage = async () => {
+    const uri = await pickImage();
+    if (uri) setImageUri(uri);
+  };
   return (
     <SafeAreaView>
       <StarInput score={score} onChangeScore={(value) => setScore(value)} />
@@ -64,6 +72,12 @@ export const CreateReviewScreen: React.FC<Props> = ({
         label="レビュー"
         placeholder="レビューを書いて下さい"
       />
+      <View style={styles.photoContainer}>
+        <IconButton name="camera" onPress={onPickImage} color="#ccc" />
+        {!!imageUri && (
+          <Image source={{ uri: imageUri }} style={styles.image} />
+        )}
+      </View>
       <Button text="レビューを投稿する" onPress={onSubmit} />
     </SafeAreaView>
   );
@@ -81,5 +95,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  photoContainer: {
+    margin: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 8,
   },
 });
